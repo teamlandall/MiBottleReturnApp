8278f9770c0136d3385e6cf138508afe0e1fce42
Kevin Kieninger <ktkienin@gmail.com>
2015-10-06 14:46:38 -0400

    fixed map mouse to zoom

--- js/map.js
+++ js/map.js
@@ -1,6 +1,8 @@
 
 L.mapbox.accessToken = 'pk.eyJ1Ijoia2tpZW5pbmdlciIsImEiOiJjaWZlM2FpYWg2ZDl4czRtNzZhb3hmcG9nIn0._7XdzBHRCxYnyGhsJgzO2w';
-var map = L.mapbox.map('map', 'kkieninger.cife3agsc6cy9rylxl7ldcgjn')
+var map = L.mapbox.map('map', 'kkieninger.cife3agsc6cy9rylxl7ldcgjn', {
+    scrollWheelZoom: false
+})
     .setView([42.347, -83.062], 13)
     .addControl(L.mapbox.geocoderControl('mapbox.places'));
 
