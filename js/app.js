67c8113225c2dbd0a3dd8a4e993a0f69019a21bd
Landall Proctor <landallproctor@gmail.com>
2015-10-12 16:38:25 -0400

    Added all views to finish story line through payment of both hoarder and contractor.

--- js/app.js
+++ js/app.js
@@ -41,6 +41,27 @@ app.config(function($routeProvider){
     controller: 'contractorConfirmController'
   });
 
+    //hoarder view. Contractor confirmed pick up.
+  $routeProvider.when('/pickUpConfirmed', {
+    templateUrl: 'partials/pickUpConfirmed.html',
+    controller: 'pickUpConfirmedController'
+  });
+
+  $routeProvider.when('/hoarderPayment', {
+    templateUrl: 'partials/hoarderPayment.html',
+    controller: 'hoarderPaymentController'
+  });
+
+  $routeProvider.when('/confirmQty', {
+    templateUrl: 'partials/confirmQty.html',
+    controller: 'confirmQtyController'
+  });
+
+  $routeProvider.when('/contractorPayment', {
+    templateUrl: 'partials/contractorPayment.html',
+    controller: 'contractorPaymentController'
+  });
+
    $routeProvider.otherwise({
     templateUrl: 'partials/404.html',
     controller: '404controller'
@@ -53,8 +74,24 @@ app.controller('landingViewController', function(){
 
 });
 
-app.controller('hoarderConfirmController', function(){
+app.controller('hoarderConfirmController', ['getSignInHoarder', '$scope', function(getSignInHoarder, $scope){
+   $scope.hUserName = getSignInHoarder.toDisplay();
+}]);
 
+app.controller('pickUpConfirmedController', function(){
+  //hoarder view. Contractor confirmed pick up.
+});
+
+app.controller('hoarderPaymentController', function(){
+  //hoarder view. payment after contractor pick up.
+});
+
+app.controller('contractorPaymentController', function(){
+  //hoarder view. payment after contractor pick up.
+});
+
+app.controller('confirmQtyController', function(){
+  //Contractor View. Confirm Qty - Rate the Hoarder.
 });
 
 
